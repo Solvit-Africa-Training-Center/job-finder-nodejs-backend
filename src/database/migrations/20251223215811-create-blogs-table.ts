81a87b80588f75c0fe423cfa45f70a8bcb5e7d21
KABANOFESTO <kabanofesto1@gmail.com>
2025-12-24 00:10:09 +0200

    ch: add migrations

---
+++ src/database/migrations/20251223215811-create-blogs-table.ts
@@ -0,0 +1,37 @@
+import { QueryInterface, DataTypes } from 'sequelize';
+
+export default {
+  async up(queryInterface: QueryInterface) {
+    await queryInterface.createTable('blogs', {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      imageUrl: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface: QueryInterface) {
+    await queryInterface.dropTable('blogs');
+  },
+};
