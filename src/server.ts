c994e6fc26d1ca2a6be54ade31d7e57aea719c93
Bertrand-mg <bertrandmutabazig@gmail.com>
2025-12-25 18:32:30 +0200

    Configured cors helmet rate limit

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,24 @@
 import express, { Express } from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 import { config as Dotenv } from 'dotenv';
 Dotenv();
 import { configs } from './config';
 import { connectToDb } from './database';
 import mainRoute from './routes';
+import { applyRateLimit } from './middlewares';
 
 const app: Express = express();
 
 const startApp = async () => {
   try {
+    app.use(helmet());
+    applyRateLimit(app);
     app.use(cors());
     app.use(express.json());
+
     await connectToDb();
+
     app.use(configs.prefix, mainRoute);
 
     app.listen(configs.port, () =>
