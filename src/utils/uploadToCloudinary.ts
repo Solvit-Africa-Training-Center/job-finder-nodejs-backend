8e28c010beebe13b8a3c686387f9bbe546abe209
TUYISHIMEMaximillien75 <tuyishimemaximillien@gmail.com>
2026-01-06 09:02:10 +0200

    ft file ulpoad on cloudinary configured and test result is 200, file model initialized and configured to upload file and migrated

--- src/utils/uploadToCloudinary.ts
+++ src/utils/uploadToCloudinary.ts
@@ -1,39 +1,20 @@
 import cloudinary from '../config/cloudinary';
 
-type UploadResult = {
-  secureUrl: string;
-  publicId: string;
-  resourceType: 'image' | 'video' | 'raw';
-};
-
 export const uploadToCloudinary = (
   buffer: Buffer,
   folder: string,
-  mimeType: string,
-): Promise<UploadResult> => {
+): Promise<{ secureUrl: string; publicId: string }> => {
   return new Promise((resolve, reject) => {
     if (!buffer) {
       return reject(new Error('File buffer is required'));
     }
 
-    if (!mimeType) {
-      return reject(new Error('File mimeType is required'));
-    }
-
-    let resourceType: 'image' | 'video' | 'raw' = 'raw';
-
-    if (mimeType.startsWith('image/')) {
-      resourceType = 'image';
-    } else if (mimeType.startsWith('video/')) {
-      resourceType = 'video';
-    }
-
     const uploadStream = cloudinary.uploader.upload_stream(
       {
         folder,
-        resource_type: resourceType,
+        resource_type: 'auto', // auto = image, video, raw
       },
-      (error, result) => {
+      (error: any, result: any) => {
         if (error) return reject(error);
 
         if (!result) {
@@ -43,7 +24,6 @@ export const uploadToCloudinary = (
         resolve({
           secureUrl: result.secure_url,
           publicId: result.public_id,
-          resourceType,
         });
       },
     );
@@ -51,9 +31,3 @@ export const uploadToCloudinary = (
     uploadStream.end(buffer);
   });
 };
-//      *
-//     * *
-//    * | *   Other team members will not need knowledge aboyt cloudinary
-//   *  |  *   The just need to provide {buffer, folder, mimeType}
-//  *   .   *
-// ***********
